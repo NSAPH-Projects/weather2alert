a20b5d06d2cdb93c3b2102bf51f86395a5354c80
mauricio <mauriciogtec@gmail.com>
2024-09-03 19:29:15 -0400

    improved setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,23 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
-    name="broach",
+    name="weather2alert",
     version="0.1.0",
-    description="Bayesian Rewards Over Actual Climate History",
+    license="MIT",
+    description="A gym environment for optimizing heat alert issuance during heatwaves",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author="Anonymous",
     author_email="actual.marmots.0d@icloud.com",  # temporarily anonymous
     # url="https://github.com/your-username/your-repo",  # temporarily anonymous
     packages=find_packages(),
+    package_data={
+        "weather2alert": ["weights"]
+    },
+    include_package_data=True,
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
@@ -17,5 +27,6 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
     ],
+    python_requires=">=3.10",
     install_requires=open("requirements.txt").read().splitlines(),
 )
